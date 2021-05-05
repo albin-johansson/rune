754fb34a703008f1a8e49b35271686efdf70d69e
Albin Johansson <albinoscar99@hotmail.com>
2021-05-05 14:28:59 +0200

    Update game.hpp

--- src/rune/game.hpp
+++ src/rune/game.hpp
@@ -1,1 +1,36 @@
 #pragma once
+
+#include <concepts>  // convertible_to
+
+#include "delta_time.hpp"
+#include "graphics.hpp"
+#include "input.hpp"
+
+namespace rune {
+
+// clang-format off
+
+template <typename T>
+concept game_type = requires (T game, const input& input, graphics& graphics, delta_time dt)
+{
+  { game.handle_input(input) };
+  { game.tick(dt) };
+  { game.render(graphics) };
+  { game.should_quit() } -> std::convertible_to<bool>;
+};
+
+template <typename T>
+concept has_on_start = requires (T game)
+{
+  { game.on_start() };
+};
+
+template <typename T>
+concept has_on_exit = requires (T game)
+{
+  { game.on_exit() };
+};
+
+// clang-format on
+
+}  // namespace rune
