3d9a77df8ae95e7b36d78fc5a64ad349820902d4
Albin Johansson <albinoscar99@hotmail.com>
2021-05-05 23:50:28 +0200

    Update tmx_local_id.hpp

--- src/rune/tmx/tmx_local_id.hpp
+++ src/rune/tmx/tmx_local_id.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
-#include <nenya.hpp>
+#include <nenya.hpp>  // strong_type
 
 namespace rune {
-namespace tags {
-struct local_id;
-}
 
-using tmx_local_id = nenya::strong_type<int, tags::local_id>;
+namespace tags {
+struct tmx_local_id_tag;
+}  // namespace tags
+
+using tmx_local_id = nenya::strong_type<int, tags::tmx_local_id_tag>;
 
 }  // namespace rune
