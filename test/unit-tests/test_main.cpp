b19729f6566cc27ef9259693b6132eec8b0f3e85
Albin Johansson <albinoscar99@hotmail.com>
2021-05-05 22:12:32 +0200

    Update test_main.cpp

--- test/unit-tests/test_main.cpp
+++ test/unit-tests/test_main.cpp
@@ -4,7 +4,13 @@
 
 int main(int argc, char** argv)
 {
-  cen::library centurion;
+  // Don't initialize audio because: linux (-.-)
+  cen::config cfg;
+  cfg.initMixer = false;
+  cfg.coreFlags = SDL_INIT_EVERYTHING & ~SDL_INIT_AUDIO;
+
+  cen::library centurion{cfg};
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
