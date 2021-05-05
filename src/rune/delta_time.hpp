771703de9cab9cd0d6cdadc0e2b582fa30928291
Albin Johansson <albinoscar99@hotmail.com>
2021-05-05 13:30:46 +0200

    Create delta_time.hpp

---
+++ src/rune/delta_time.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace rune {
+
+#ifndef RUNE_DELTA_TIME_TYPE
+#define RUNE_DELTA_TIME_TYPE float
+#endif  // RUNE_DELTA_TIME_TYPE
+
+using delta_time = RUNE_DELTA_TIME_TYPE;
+
+}  // namespace rune
