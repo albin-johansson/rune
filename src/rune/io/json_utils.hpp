c3e0749c9d0c85f0c5cb03fb291f059ca1be721a
Albin Johansson <albinoscar99@hotmail.com>
2021-05-06 12:43:16 +0200

    Update json_utils.hpp

--- src/rune/io/json_utils.hpp
+++ src/rune/io/json_utils.hpp
@@ -1,12 +1,24 @@
 #pragma once
 
 #include <json.hpp>     // json
+#include <optional>     // optional
 #include <string_view>  // string_view
 
 namespace rune {
 
 template <typename T>
 void get_if_exists(const nlohmann::json& json, const std::string_view key, T& value)
+{
+  if (const auto it = json.find(key); it != json.end())
+  {
+    value = it->get<T>();
+  }
+}
+
+template <typename T>
+void get_if_exists(const nlohmann::json& json,
+                   const std::string_view key,
+                   std::optional<T>& value)
 {
   if (const auto it = json.find(key); it != json.end())
   {
