728b3b5364f638d46ce731a3ce7e5d73599c7993
Albin Johansson <albinoscar99@hotmail.com>
2021-05-08 02:11:17 +0200

    Update integers.hpp

--- src/rune/aliases/integers.hpp
+++ src/rune/aliases/integers.hpp
@@ -5,18 +5,40 @@
 
 namespace rune {
 
-using cen::uint;
-using cen::ulonglong;
+/// \addtogroup core
+/// \{
 
+/// Unsigned integer.
+using uint = cen::uint;
+
+/// Used as the argument type to integral literal operators.
+using ulonglong = cen::ulonglong;
+
+/// 8-bit signed integer.
 using int8 = cen::i8;
+
+/// 16-bit signed integer.
 using int16 = cen::i16;
+
+/// 32-bit signed integer.
 using int32 = cen::i32;
+
+/// 64-bit signed integer.
 using int64 = cen::i64;
 
+/// 8-bit unsigned integer.
 using uint8 = cen::u8;
+
+/// 16-bit unsigned integer.
 using uint16 = cen::u16;
+
+/// 32-bit unsigned integer.
 using uint32 = cen::u32;
+
+/// 64-bit unsigned integer.
 using uint64 = cen::u64;
+
+/// \} End of group core
 
 }  // namespace rune
 
