455e0ee3409c7ec96ed53be6f44e254e93a6cfe6
Albin Johansson <albinoscar99@hotmail.com>
2021-05-08 02:11:12 +0200

    Update delta_time.hpp

--- src/rune/aliases/delta_time.hpp
+++ src/rune/aliases/delta_time.hpp
@@ -7,6 +7,16 @@ namespace rune {
 #define RUNE_DELTA_TIME_TYPE float
 #endif  // RUNE_DELTA_TIME_TYPE
 
+/**
+ * \brief The type used for delta time values, e.g. in the `tick()` function of game class
+ * implementations.
+ *
+ * \details By default, this alias is equivalent to `float`.
+ *
+ * \ingroup core
+ *
+ * \see `RUNE_DELTA_TIME_TYPE`
+ */
 using delta_time = RUNE_DELTA_TIME_TYPE;
 
 }  // namespace rune
