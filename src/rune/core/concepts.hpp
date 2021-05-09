1ad53376a32937f39bb1dfa7c4f5718abbc37716
Albin Johansson <albinoscar99@hotmail.com>
2021-05-10 00:01:50 +0200

    Update concepts.hpp

--- src/rune/core/concepts.hpp
+++ src/rune/core/concepts.hpp
@@ -1,24 +1,27 @@
 #ifndef RUNE_CORE_CONCEPTS_HPP
 #define RUNE_CORE_CONCEPTS_HPP
 
-#include <type_traits>  // is_arithmetic_v, is_same_v
+#include <type_traits>  // is_arithmetic_v, is_same_v, is_default_constructible_v
 
 namespace rune {
 
 /// \addtogroup core
 /// \{
 
+// clang-format off
+
 /// Concept for a type that is either integral or floating-point, but not `bool`.
 template <typename T>
 concept numeric = std::is_arithmetic_v<T> && !std::is_same_v<T, bool>;
-
-// clang-format off
 
 template <typename T>
 concept has_value_type = requires
 {
   typename T::value_type;
 };
+
+template <typename T>
+concept default_constructible = std::is_default_constructible_v<T>;
 
 // clang-format on
 
