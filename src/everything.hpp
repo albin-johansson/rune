3c7683c4be4ff5c10b12d615f3c5acef03b53d6c
Albin Johansson <albinoscar99@hotmail.com>
2021-05-11 00:14:41 +0200

    Add random number generation functions

--- src/everything.hpp
+++ src/everything.hpp
@@ -47,6 +47,7 @@
 #include "rune/math/almost_equal.hpp"
 #include "rune/math/max.hpp"
 #include "rune/math/min.hpp"
+#include "rune/math/random_utils.hpp"
 #include "rune/math/vector2.hpp"
 #include "rune/tmx/parse_tileset.hpp"
 #include "rune/tmx/tmx.hpp"
