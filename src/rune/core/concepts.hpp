67675e1560a5613c65be9ddad1e5e6a45f0904b2
Albin Johansson <albinoscar99@hotmail.com>
2021-05-10 14:42:58 +0200

    Add has_less_than concept

--- src/rune/core/concepts.hpp
+++ src/rune/core/concepts.hpp
@@ -1,7 +1,8 @@
 #ifndef RUNE_CORE_CONCEPTS_HPP
 #define RUNE_CORE_CONCEPTS_HPP
 
-#include <type_traits>  // is_arithmetic_v, is_same_v, is_default_constructible_v
+#include <concepts>     // convertible_to, same_as
+#include <type_traits>  // is_arithmetic_v, is_default_constructible_v
 
 namespace rune {
 
@@ -12,7 +13,10 @@ namespace rune {
 
 /// Concept for a type that is either integral or floating-point, but not `bool`.
 template <typename T>
-concept numeric = std::is_arithmetic_v<T> && !std::is_same_v<T, bool>;
+concept numeric = std::is_arithmetic_v<T> && !std::same_as<T, bool>;
+
+template <typename T>
+concept default_constructible = std::is_default_constructible_v<T>;
 
 template <typename T>
 concept has_value_type = requires
@@ -21,7 +25,10 @@ concept has_value_type = requires
 };
 
 template <typename T>
-concept default_constructible = std::is_default_constructible_v<T>;
+concept has_less_than = requires (T value)
+{
+  { value < value } -> std::convertible_to<bool>;
+};
 
 // clang-format on
 
