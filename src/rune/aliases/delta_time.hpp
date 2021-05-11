4826fb67860780e470a560a4a5de6323d30c9285
Albin Johansson <albinoscar99@hotmail.com>
2021-05-11 22:08:13 +0200

    Make delta_time a strong type

--- src/rune/aliases/delta_time.hpp
+++ src/rune/aliases/delta_time.hpp
@@ -1,23 +1,41 @@
 #ifndef RUNE_ALIASES_DELTA_TIME_HPP
 #define RUNE_ALIASES_DELTA_TIME_HPP
 
+#include <nenya.hpp>  // strong_type
+
 namespace rune {
 
-#ifndef RUNE_DELTA_TIME_TYPE
-#define RUNE_DELTA_TIME_TYPE float
-#endif  // RUNE_DELTA_TIME_TYPE
+/// \cond FALSE
+namespace tags {
+struct delta_time_tag;
+}
+/// \endcond
+
+/// \addtogroup core
+/// \{
+
+/**
+ * \def RUNE_DELTA_TIME_UNDERLYING_TYPE
+ *
+ * \brief The underlying type of the `delta_time` strong type.
+ *
+ * \note The underlying should be a floating-point type, i.e. `float`, `double` or
+ * possibly `long double`.
+ */
+#ifndef RUNE_DELTA_TIME_UNDERLYING_TYPE
+#define RUNE_DELTA_TIME_UNDERLYING_TYPE float
+#endif  // RUNE_DELTA_TIME_UNDERLYING_TYPE
 
 /**
  * \brief The type used for delta time values, e.g. in the `tick()` function of game class
  * implementations.
  *
- * \details By default, this alias is equivalent to `float`.
- *
- * \ingroup core
- *
- * \see `RUNE_DELTA_TIME_TYPE`
+ * \see `RUNE_DELTA_TIME_UNDERLYING_TYPE`
  */
-using delta_time = RUNE_DELTA_TIME_TYPE;
+using delta_time =
+    nenya::strong_type<RUNE_DELTA_TIME_UNDERLYING_TYPE, tags::delta_time_tag>;
+
+/// \} End of group core
 
 }  // namespace rune
 
