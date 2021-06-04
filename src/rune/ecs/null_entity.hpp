c24e45747734778223b0994b590bdbeb50e92fa5
Albin Johansson <albinoscar99@hotmail.com>
2021-06-04 16:41:57 +0200

    Create null_entity.hpp

---
+++ src/rune/ecs/null_entity.hpp
@@ -0,0 +1,28 @@
+#ifndef RUNE_ECS_NULL_ENTITY_HPP
+#define RUNE_ECS_NULL_ENTITY_HPP
+
+#include <concepts>  // constructible_from
+#include <entt.hpp>  // entity, null
+
+namespace rune {
+
+// clang-format off
+
+template <typename T>
+[[nodiscard]] constexpr auto null()
+    noexcept(noexcept(typename T::entity{entt::entity{entt::null}}))
+{
+  return typename T::entity{entt::entity{entt::null}};
+}
+
+// clang-format on
+
+template <std::constructible_from<entt::entity> T>
+void nullify(T& entity) noexcept(noexcept(T{entt::entity{entt::null}}))
+{
+  entity = T{entt::entity{entt::null}};
+}
+
+}  // namespace rune
+
+#endif  // RUNE_ECS_NULL_ENTITY_HPP
