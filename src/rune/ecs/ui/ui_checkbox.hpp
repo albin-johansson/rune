e4aab0d0bc20731c997349bf22133355b32d6d08
Albin Johansson <albinoscar99@hotmail.com>
2021-06-04 16:42:17 +0200

    Create ui_checkbox.hpp

---
+++ src/rune/ecs/ui/ui_checkbox.hpp
@@ -0,0 +1,26 @@
+#ifndef RUNE_ECS_UI_CHECKBOX_HPP
+#define RUNE_ECS_UI_CHECKBOX_HPP
+
+#include "../entity_type.hpp"
+
+namespace rune {
+
+namespace tags {
+struct ui_checkbox_tag;
+}  // namespace tags
+
+struct ui_checkbox final
+{
+  using entity = entity_type<tags::ui_checkbox_tag>;
+
+  bool is_checked{};
+};
+
+void serialize(auto& archive, ui_checkbox& cb)
+{
+  archive(cb.is_checked);
+}
+
+}  // namespace rune
+
+#endif  // RUNE_ECS_UI_CHECKBOX_HPP
