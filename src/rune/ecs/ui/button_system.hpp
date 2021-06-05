9abb483a0b6e78b84b99ee632f02a608904579f8
Albin Johansson <albinoscar99@hotmail.com>
2021-06-05 16:50:34 +0200

    Update button_system.hpp

--- src/rune/ecs/ui/button_system.hpp
+++ src/rune/ecs/ui/button_system.hpp
@@ -3,21 +3,47 @@
 
 #include <centurion.hpp>  // mouse
 #include <entt.hpp>       // registry, dispatcher
+#include <string>         // string
+#include <utility>        // move
 
+#include "../../aliases/integers.hpp"
 #include "../../aliases/maybe.hpp"
+#include "../../core/graphics.hpp"
 #include "ui_bounds.hpp"
 #include "ui_button.hpp"
 #include "ui_checkbox.hpp"
+#include "ui_label.hpp"
+#include "ui_position.hpp"
 
 namespace rune {
 
-inline auto make_button(entt::registry& registry) -> ui_button::entity
+struct ui_button_cfg final
+{
+  float row{};
+  float column{};
+  std::string text;
+  uint32 id{};
+  cen::color fg{cen::colors::white};
+};
+
+inline auto make_button(entt::registry& registry, ui_button_cfg cfg) -> ui_button::entity
 {
   const auto entity = ui_button::entity{registry.create()};
 
   auto& button = registry.emplace<ui_button>(entity);
+  button.id = cfg.id;
+
+  auto& label = registry.emplace<ui_label>(entity);
+  label.text = std::move(cfg.text);
+  // TODO label color
+
+  auto& position = registry.emplace<ui_position>(entity);
+  position.row = cfg.row;
+  position.col = cfg.column;
+
   auto& bounds = registry.emplace<ui_bounds>(entity);
-  // TODO ui_label
+
+  // TODO set bounds position
 
   return entity;
 }
@@ -34,7 +60,7 @@ inline auto make_button(entt::registry& registry) -> ui_button::entity
 
     if (mouse.was_left_button_released())
     {
-      dispatcher.trigger<button_pressed_event>(entity, b.id);
+      dispatcher.trigger<button_pressed_event>(entity, button.id);
       button.is_hovered = false;
 
       if (auto* checkbox = registry.try_get<ui_checkbox>(entity))
@@ -51,19 +77,20 @@ inline auto make_button(entt::registry& registry) -> ui_button::entity
   return false;
 }
 
-auto update_button_hover(entt::registry& registry, const cen::mouse& mouse)
+inline auto update_button_hover(entt::registry& registry, const cen::mouse& mouse)
     -> maybe<ui_button::entity>
 {
-  for (auto&& [entity, button] : registry.view<ui_button>().each())
+  const auto mousePos = cen::cast<cen::fpoint>(mouse.position());
+  for (auto&& [entity, button, bounds] :
+       registry.view<ui_button, const ui_bounds>().each())
   {
     if (button.is_visible)
     {
-      //      const auto& drawable = registry.get<comp::button_drawable>(entity);
-      //      button.hover = drawable.bounds.contains(mousePos);
-      //      if (button.hover)
-      //      {
-      //        return comp::button::entity{entity};
-      //      }
+      button.is_hovered = bounds.bounds.contains(mousePos);
+      if (button.is_hovered)
+      {
+        return ui_button::entity{entity};
+      }
     }
   }
 
