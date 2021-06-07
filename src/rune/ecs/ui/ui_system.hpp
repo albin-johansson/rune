f1bc866d6a648d4b4f7f0cdb9a92a3db3236e708
Albin Johansson <albinoscar99@hotmail.com>
2021-06-07 19:31:23 +0200

    Update ui_system.hpp

--- src/rune/ecs/ui/ui_system.hpp
+++ src/rune/ecs/ui/ui_system.hpp
@@ -4,20 +4,33 @@
 #include <entt.hpp>  // registry, dispatcher
 
 #include "../../core/graphics.hpp"
+#include "../../core/input.hpp"
+#include "button_system.hpp"
 #include "label_system.hpp"
 #include "line_system.hpp"
+#include "ui_menu.hpp"
 
 namespace rune {
 
-inline void update_ui(entt::registry& registry, entt::dispatcher& dispatcher)
+inline void update_ui(entt::registry& registry,
+                      entt::dispatcher& dispatcher,
+                      const input& input)
 {
   // TODO check for button hover
+
+  if (const auto button = update_button_hover(registry, input.mouse))
+  {
+    if (query_button(registry, dispatcher, *button, input.mouse))
+    {
+    }
+  }
   // TODO key binds
 }
 
 inline void render_ui(const entt::registry& registry, graphics& gfx)
 {
   render_lines(registry, gfx);
+  render_buttons(registry, gfx);
   render_labels(registry, gfx);
 }
 
