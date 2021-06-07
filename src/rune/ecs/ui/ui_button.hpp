2afcea050f23b0979b3573a4bc87b9c4f2d7de97
Albin Johansson <albinoscar99@hotmail.com>
2021-06-07 19:30:43 +0200

    Update ui_button.hpp

--- src/rune/ecs/ui/ui_button.hpp
+++ src/rune/ecs/ui/ui_button.hpp
@@ -1,7 +1,10 @@
 #ifndef RUNE_ECS_UI_UI_BUTTON_HPP
 #define RUNE_ECS_UI_UI_BUTTON_HPP
 
+#include <centurion.hpp>  // farea
+
 #include "../../aliases/integers.hpp"
+#include "../../aliases/maybe.hpp"
 #include "../entity_type.hpp"
 
 namespace rune {
@@ -15,6 +18,7 @@ struct ui_button final
   using entity = entity_type<tags::ui_button_tag>;
 
   uint32 id{};
+  mutable maybe<cen::farea> size;
   bool is_enabled{true};
   bool is_visible{true};
   bool is_hovered{false};
@@ -22,7 +26,7 @@ struct ui_button final
 
 void serialize(auto& archive, ui_button& b)
 {
-  archive(b.id, b.is_enabled, b.is_visible, b.is_hovered);
+  archive(b.id, b.size, b.is_enabled, b.is_visible, b.is_hovered);
 }
 
 }  // namespace rune
