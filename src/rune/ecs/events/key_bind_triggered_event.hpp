aaf9fdfc5ef1bafb5dcf3268a6657e96e72b3a8e
Albin Johansson <albinoscar99@hotmail.com>
2021-07-27 22:24:43 +0200

    Update key_bind_triggered_event.hpp

--- src/rune/ecs/events/key_bind_triggered_event.hpp
+++ src/rune/ecs/events/key_bind_triggered_event.hpp
@@ -10,10 +10,17 @@ namespace rune {
 /// \addtogroup ecs
 /// \{
 
+/**
+ * \struct key_bind_triggered_event
+ *
+ * \brief An event that is emitted whenever a key bind is triggered.
+ *
+ * \since 0.1.0
+ */
 struct key_bind_triggered_event final
 {
-  ui_key_bind::entity entity{null<ui_key_bind>()};
-  uint32 id{};
+  ui_key_bind::entity entity{null<ui_key_bind>()};  ///< The key bind entity.
+  uint32 id{};                                      ///< The key bind ID.
 };
 
 /// \} End of group ecs
