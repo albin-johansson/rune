7fb0b7f80cc42d42a4d5a412fa50ef12e26cda50
Albin Johansson <albinoscar99@hotmail.com>
2021-07-27 22:24:40 +0200

    Update button_pressed_event.hpp

--- src/rune/ecs/events/button_pressed_event.hpp
+++ src/rune/ecs/events/button_pressed_event.hpp
@@ -10,10 +10,17 @@ namespace rune {
 /// \addtogroup ecs
 /// \{
 
+/**
+ * \struct button_pressed_event
+ *
+ * \brief An event that is emitted whenever a button is pressed.
+ *
+ * \since 0.1.0
+ */
 struct button_pressed_event final
 {
   ui_button::entity button_entity{null<ui_button>()};  ///< The pressed button entity.
-  uint32 id{};  ///< The ID associated with the button.
+  uint32 id{};                                         ///< The button ID.
 };
 
 /// \} End of group ecs
