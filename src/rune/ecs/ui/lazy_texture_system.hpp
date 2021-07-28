b305959ab1c30834b33827eee6be359ba8b1a9de
Albin Johansson <albinoscar99@hotmail.com>
2021-07-28 02:30:46 +0200

    Add ui_lazy_texture component

---
+++ src/rune/ecs/ui/lazy_texture_system.hpp
@@ -0,0 +1,54 @@
+#ifndef RUNE_ECS_UI_LAZY_TEXTURE_SYSTEM_HPP
+#define RUNE_ECS_UI_LAZY_TEXTURE_SYSTEM_HPP
+
+#include <centurion.hpp>  // texture
+#include <entt.hpp>       // registry
+
+#include "../../aliases/czstring.hpp"
+#include "../../core/graphics.hpp"
+#include "ui_lazy_texture.hpp"
+
+namespace rune::ui {
+
+/// \addtogroup ecs
+/// \{
+
+/// \name Factory functions
+/// \{
+
+inline auto make_lazy_texture(entt::registry& registry, const czstring image)
+    -> ui_lazy_texture::entity
+{
+  const auto entity = ui_lazy_texture::entity{registry.create()};
+
+  registry.emplace<ui_lazy_texture>(entity, cen::surface{image});
+
+  return entity;
+}
+
+/// \} End of factory functions
+
+/// \} End of group ecs
+
+/// \cond FALSE
+
+namespace detail {
+
+inline void update_lazy_textures(const entt::registry& registry, graphics& gfx)
+{
+  for (auto&& [entity, lazy] : registry.view<ui_lazy_texture>().each())
+  {
+    if (!lazy.texture)
+    {
+      lazy.texture = cen::texture{gfx.renderer(), lazy.source};
+    }
+  }
+}
+
+}  // namespace detail
+
+/// \endcond
+
+}  // namespace rune::ui
+
+#endif  // RUNE_ECS_UI_LAZY_TEXTURE_SYSTEM_HPP
