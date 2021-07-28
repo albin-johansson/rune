a6dd63c9aa02b4ad54d3bd419f50844851e98394
Albin Johansson <albinoscar99@hotmail.com>
2021-07-28 02:44:13 +0200

    Update ui_checkbox.hpp

--- src/rune/ecs/ui/ui_checkbox.hpp
+++ src/rune/ecs/ui/ui_checkbox.hpp
@@ -12,6 +12,15 @@ namespace tags {
 struct ui_checkbox_tag;
 }  // namespace tags
 
+/**
+ * \struct ui_checkbox
+ *
+ * \brief A component that represents a UI button, i.e. a control that can be pressed.
+ *
+ * \see `serialize(auto&, ui_checkbox&)`
+ *
+ * \since 0.1.0
+ */
 struct ui_checkbox final
 {
   using entity = entity_type<tags::ui_checkbox_tag>;
