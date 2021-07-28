890e9eba4d3ecbd1fa2606f8d8d726dff23f4273
Albin Johansson <albinoscar99@hotmail.com>
2021-07-28 21:36:27 +0200

    Update game_test.cpp

--- test/unit-tests/rune/core/game_test.cpp
+++ test/unit-tests/rune/core/game_test.cpp
@@ -4,27 +4,27 @@
 
 #include "core/engine.hpp"
 
-class Game final
+class Game final : public rune::game_base
 {
  public:
   using graphics_type = rune::graphics;
 
-//  void init(graphics_type& gfx)
-//  {
-//    std::cout << "Game::init\n";
-//  }
-//
-//  void on_start()
-//  {
-//    std::cout << "Game::on_start\n";
-//  }
-//
-//  void on_exit()
-//  {
-//    std::cout << "Game::on_exit\n";
-//  }
+  //  void init(graphics_type& gfx)
+  //  {
+  //    std::cout << "Game::init\n";
+  //  }
+  //
+  //  void on_start()
+  //  {
+  //    std::cout << "Game::on_start\n";
+  //  }
+  //
+  //  void on_exit()
+  //  {
+  //    std::cout << "Game::on_exit\n";
+  //  }
 
-  void handle_input(const rune::input& input)
+  void handle_input(const rune::input& input) override
   {
     if (input.keyboard.just_released(cen::scancodes::escape))
     {
@@ -32,18 +32,21 @@ class Game final
     }
   }
 
-  void tick(rune::delta_time dt)
+  void tick(rune::delta_time dt) override
   {}
 
-  void render(rune::graphics& graphics) const
+  void render(rune::graphics& graphics) const override
   {
     auto& renderer = graphics.renderer();
+    renderer.clear_with(cen::colors::black);
 
     renderer.set_color(cen::colors::red);
     renderer.fill_rect(cen::rect(25, 25, 150, 100));
+
+    renderer.present();
   }
 
-  [[nodiscard]] auto should_quit() const -> bool
+  [[nodiscard]] auto should_quit() const -> bool override
   {
     return m_quit;
   }
