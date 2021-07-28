4bfcc9af3a8d02333b2e2344151debbde808867f
Albin Johansson <albinoscar99@hotmail.com>
2021-07-28 22:18:17 +0200

    Add compiler_test.cpp

---
+++ test/unit-tests/rune/core/compiler_test.cpp
@@ -0,0 +1,30 @@
+#include "core/compiler.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(Compiler, OnMSVC)
+{
+#ifdef _MSC_VER
+  ASSERT_TRUE(rune::on_msvc());
+#else
+  ASSERT_FALSE(rune::on_msvc());
+#endif  // _MSC_VER
+}
+
+TEST(Compiler, OnGCC)
+{
+#ifdef __GNUC__
+  ASSERT_TRUE(rune::on_gcc());
+#else
+  ASSERT_FALSE(rune::on_gcc());
+#endif  // __GNUC__
+}
+
+TEST(Compiler, OnClang)
+{
+#ifdef __clang__
+  ASSERT_TRUE(rune::on_clang());
+#else
+  ASSERT_FALSE(rune::on_clang());
+#endif  // __clang__
+}
