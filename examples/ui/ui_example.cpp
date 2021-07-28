2fd57b5b9d7fec10eb71f3a4c99c2ff5c86d489c
Albin Johansson <albinoscar99@hotmail.com>
2021-07-28 03:04:57 +0200

    Update ui_example.cpp

--- examples/ui/ui_example.cpp
+++ examples/ui/ui_example.cpp
@@ -1,5 +1,6 @@
 #include <centurion.hpp>
-#include <rune.hpp>
+
+#include "../../src/everything.hpp"
 
 namespace {
 
@@ -15,6 +16,8 @@ class ui_example final : public rune::game_base
  public:
   explicit ui_example(rune::graphics& gfx)
   {
+    gfx.renderer().set_logical_size(cen::window::default_size());
+
     // clang-format off
     m_dispatcher.sink<rune::button_pressed_event>().connect<&ui_example::on_button_pressed>(this);
     m_dispatcher.sink<rune::key_bind_triggered_event>().connect<&ui_example::on_key_bind_triggered>(this);
@@ -22,14 +25,40 @@ class ui_example final : public rune::game_base
 
     gfx.emplace_cache(0, "resources/fonts/daniel.ttf", 36);
 
-    rune::make_button(m_registry,
-                      {.position = {10, 10}, .text = "foo", .fg = cen::colors::red});
+    const auto menu =
+        rune::ui::make_menu(m_registry, {.id = 0, .title = "Menu", .is_blocking = true});
+    m_registry.set<rune::active_menu>(menu);
 
-    rune::make_button(m_registry, {.position = {16, 10}, .text = "bar"});
+    rune::ui::make_label(m_registry,
+                         menu,
+                         {.position = {4, 4},
+                          .text = "Label",
+                          .color = cen::colors::crimson,
+                          .shadow = true});
 
-    rune::make_line(m_registry, {.start = {14, 35}, .end = {16, 60}});
+    rune::ui::make_button(m_registry,
+                          menu,
+                          {.position = {10, 4},
+                           .text = "Button 1",
+                           .id = 1,
+                           .fg = cen::colors::aquamarine,
+                           .shadow = true});
 
-    rune::make_key_bind(m_registry, cen::scancodes::escape, action_id::quit);
+    rune::ui::make_button(m_registry,
+                          menu,
+                          {.position = {20, 4},
+                           .text = "Button 2",
+                           .id = 2,
+                           .fg = cen::colors::aquamarine,
+                           .shadow = true});
+
+    rune::ui::make_line(m_registry,
+                        menu,
+                        {.start = {10, 40}, .end = {15, 45}, .color = cen::colors::cyan});
+
+    rune::ui::make_key_bind(m_registry, cen::scancodes::escape, action_id::quit);
+    rune::ui::make_key_bind(m_registry, cen::scancodes::f, action_id::foo);
+    rune::ui::make_key_bind(m_registry, cen::scancodes::b, action_id::bar);
   }
 
   void handle_input(const rune::input& input) override
@@ -40,10 +69,10 @@ class ui_example final : public rune::game_base
   void render(rune::graphics& gfx) const override
   {
     auto& renderer = gfx.renderer();
-    renderer.clear_with(cen::colors::black);
+    renderer.clear_with(cen::colors::teal);
 
     rune::ui::render(m_registry, gfx);
-    //    rune::debug_ui(m_registry, gfx);
+    rune::ui::debug(m_registry, gfx);
 
     renderer.present();
   }
@@ -65,11 +94,18 @@ class ui_example final : public rune::game_base
 
   void on_key_bind_triggered(const rune::key_bind_triggered_event& event)
   {
-    cen::log::info("on_key_bind_triggered");
     switch (static_cast<action_id>(event.id))
     {
       case action_id::quit:
         m_quit = true;
+        break;
+
+      case action_id::foo:
+        cen::log::info("foo");
+        break;
+
+      case action_id::bar:
+        cen::log::info("bar");
         break;
     }
   }
