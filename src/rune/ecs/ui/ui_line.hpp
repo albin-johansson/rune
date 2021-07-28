e82ee6593acaf28cff7f4e07d6a77b84d6cef139
Albin Johansson <albinoscar99@hotmail.com>
2021-07-28 03:10:47 +0200

    Update ui_line.hpp

--- src/rune/ecs/ui/ui_line.hpp
+++ src/rune/ecs/ui/ui_line.hpp
@@ -13,6 +13,16 @@ namespace tags {
 struct ui_line_tag;
 }  // namespace tags
 
+/**
+ * \struct ui_line
+ *
+ * \brief A component that represents a UI line.
+ *
+ * \see `ui::make_line()`
+ * \see `serialize(auto&, ui_line&)`
+ *
+ * \since 0.1.0
+ */
 struct ui_line final
 {
   using entity = entity_type<tags::ui_line_tag>;
