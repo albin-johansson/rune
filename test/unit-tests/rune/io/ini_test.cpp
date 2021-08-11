7aafc99fc9e2046f95360e181116d7438dec8b0a
Albin Johansson <albinoscar99@hotmail.com>
2021-08-12 01:39:48 +0200

    Overhaul basic_ini class

--- test/unit-tests/rune/io/ini_test.cpp
+++ test/unit-tests/rune/io/ini_test.cpp
@@ -12,11 +12,30 @@ TEST(Ini, Defaults)
   ASSERT_TRUE(file);
 }
 
+TEST(Ini, EmplaceOrReplace)
+{
+  rune::ini_file file;
+
+  {
+    auto& foo = file.emplace_or_replace("foo");
+    foo["abc"] = 42;
+
+    ASSERT_TRUE(file.contains("foo"));
+    ASSERT_EQ(1, file.size());
+  }
+
+  {
+    auto& foo = file.emplace_or_replace("foo");
+    ASSERT_EQ(1, file.size());
+
+    // Ensure that the section was replaced
+    ASSERT_FALSE(foo.contains("abc"));
+  }
+}
+
 TEST(Ini, GetOrEmplace)
 {
   rune::ini_file file;
-  ASSERT_EQ(0, file.size());
-  ASSERT_TRUE(file.empty());
 
   {
     auto& foo = file.get_or_emplace("foo");
@@ -28,7 +47,6 @@ TEST(Ini, GetOrEmplace)
 
   {
     auto& foo = file.get_or_emplace("foo");
-    ASSERT_EQ(1, foo.size());
     ASSERT_TRUE(foo.contains("abc"));
 
     ASSERT_TRUE(file.contains("foo"));
@@ -40,47 +58,44 @@ TEST(Ini, GetOrEmplace)
   ASSERT_EQ(2, file.size());
 }
 
-TEST(Ini, EmplaceOrReplace)
+TEST(Ini, SubscriptOperator)
 {
   rune::ini_file file;
 
-  {
-    auto& foo = file.emplace_or_replace("foo");
-    foo["abc"] = "123";
-    ASSERT_TRUE(foo.contains("abc"));
-  }
-
-  {
-    // Ensure that the section was replaced
-    auto& foo = file.emplace_or_replace("foo");
-    ASSERT_FALSE(foo.contains("abc"));
-  }
-
-  ASSERT_TRUE(file);
-  ASSERT_EQ(1, file.size());
+  file["foo"]["abc"] = 42;
   ASSERT_TRUE(file.contains("foo"));
+  ASSERT_EQ(1, file.size());
+
+  file["foo"]["def"] = 123;
+  ASSERT_TRUE(file.contains("foo"));
+  ASSERT_EQ(1, file.size());
+  ASSERT_EQ(2, file["foo"].size());
+
+  file["bar"];
+  ASSERT_EQ(2, file.size());
 }
 
 TEST(Ini, Erase)
 {
   rune::ini_file file;
-  ASSERT_NO_THROW(file.erase("foo"));
+  ASSERT_FALSE(file.erase("foo"));
 
-  file.emplace_or_replace("foo");
-  ASSERT_EQ(1, file.size());
-  ASSERT_FALSE(file.empty());
+  file["foo"];
   ASSERT_TRUE(file.contains("foo"));
+  ASSERT_EQ(1, file.size());
 
-  file.erase("foo");
+  ASSERT_TRUE(file.erase("foo"));
+  ASSERT_FALSE(file.contains("foo"));
   ASSERT_EQ(0, file.size());
   ASSERT_TRUE(file.empty());
-  ASSERT_FALSE(file.contains("foo"));
+
+  ASSERT_FALSE(file.erase("foo"));
 }
 
 TEST(Ini, At)
 {
   rune::ini_file file;
-  ASSERT_ANY_THROW(file.at("foo"));
+  ASSERT_THROW(file.at("foo"), rune::rune_error);
 
   file["foo"];
   file["bar"];
@@ -89,82 +104,113 @@ TEST(Ini, At)
   ASSERT_NO_THROW(file.at("bar"));
 }
 
-TEST(Ini, Subscript)
+TEST(Ini, Contains)
 {
   rune::ini_file file;
+  ASSERT_FALSE(file.contains("abc"));
 
   file["foo"];
   ASSERT_TRUE(file.contains("foo"));
-  ASSERT_EQ(1, file.size());
 
-  file["foo"]["bar"];
-  ASSERT_TRUE(file.contains("foo"));
-  ASSERT_EQ(1, file.size());
+  file.erase("foo");
+  ASSERT_FALSE(file.contains("foo"));
 }
 
-TEST(Ini, FromFile)
+TEST(Ini, ReadIni)
 {
-  auto file = rune::read_ini("resources/test.ini");
-  file.write(std::cout);
+  const auto file = rune::read_ini("resources/test.ini").value();
+
+  std::cout << "resources/test.ini:\n";
+  file.dump(std::cout);
 
   ASSERT_EQ(2, file.size());
   ASSERT_FALSE(file.empty());
 
-  {
-    const auto& foo = file.at("foo");
-    ASSERT_EQ(4, foo.size());
+  const auto& foo = file.at("foo");
+  ASSERT_EQ(5, foo.size());
 
-    ASSERT_TRUE(foo.contains("abc"));
-    ASSERT_EQ(rune::ini_value{"world"}, foo.at("abc"));
+  ASSERT_TRUE(foo.contains("str_1"));
+  ASSERT_TRUE(foo.at("str_1").is_string());
+  ASSERT_EQ("hello12.3", foo.at("str_1"));
 
-    ASSERT_TRUE(foo.contains("bar"));
-    ASSERT_EQ(rune::ini_value{"hello"}, foo.at("bar"));
+  ASSERT_TRUE(foo.contains("str_2"));
+  ASSERT_TRUE(foo.at("str_2").is_string());
+  ASSERT_EQ("12a", foo.at("str_2"));
 
-    ASSERT_TRUE(foo.contains("health"));
-    ASSERT_EQ(rune::ini_value{87u}, foo.at("health"));
+  ASSERT_TRUE(foo.contains("unsigned"));
+  ASSERT_TRUE(foo.at("unsigned").is_uint());
+  ASSERT_EQ(87u, foo.at("unsigned"));
 
-    ASSERT_TRUE(foo.contains("offset"));
-    ASSERT_EQ(rune::ini_value{-42}, foo.at("offset"));
-  }
+  ASSERT_TRUE(foo.contains("positive"));
+  ASSERT_TRUE(foo.at("positive").is_int());
+  ASSERT_EQ(123, foo.at("positive"));
 
-  {
-    const auto& bar = file.at("bar");
-    ASSERT_EQ(3, bar.size());
+  ASSERT_TRUE(foo.contains("negative"));
+  ASSERT_TRUE(foo.at("negative").is_int());
+  ASSERT_EQ(-42, foo.at("negative"));
 
-    ASSERT_TRUE(bar.contains("isCool"));
-    ASSERT_EQ(rune::ini_value{true}, bar.at("isCool"));
+  const auto& bar = file.at("bar");
+  ASSERT_EQ(3, bar.size());
 
-    ASSERT_TRUE(bar.contains("isFancy"));
-    ASSERT_EQ(rune::ini_value{false}, bar.at("isFancy"));
+  ASSERT_TRUE(bar.contains("real"));
+  ASSERT_TRUE(bar.at("real").is_float());
+  ASSERT_EQ(32.1, bar.at("real"));
 
-    ASSERT_TRUE(bar.contains("123"));
-    ASSERT_EQ(rune::ini_value{321}, bar.at("123"));
-  }
+  ASSERT_TRUE(bar.contains("bool_t"));
+  ASSERT_TRUE(bar.at("bool_t").is_bool());
+  ASSERT_EQ(true, bar.at("bool_t"));
+
+  ASSERT_TRUE(bar.contains("bool_f"));
+  ASSERT_TRUE(bar.at("bool_f").is_bool());
+  ASSERT_EQ(false, bar.at("bool_f"));
 }
 
-TEST(Ini, Usage)
+TEST(Ini, WriteRead)
 {
-  rune::ini_file file;
-
-  file["foo"]["abc"] = "hello";
-  file["foo"]["def"] = "world";
-  ASSERT_TRUE(file.contains("foo"));
-  ASSERT_FALSE(file.contains("bar"));
-
   {
-    const auto& foo = file.at("foo");
-    ASSERT_EQ(2, foo.size());
-    ASSERT_EQ(rune::ini_value{"hello"}, foo.at("abc"));
-    ASSERT_EQ(rune::ini_value{"world"}, foo.at("def"));
+    rune::ini_file ini;
+    ini["foo"]["positive"] = 9'394;
+    ini["foo"]["negative"] = -834;
+    ini["foo"]["unsigned"] = 734u;
+    ini["foo"]["str"] = "foobar";
+
+    ini["bar"]["real_1"] = 1.0;
+    ini["bar"]["real_2"] = 87.3;
+    ini["bar"]["real_3"] = 0.0;
+    ini["bar"]["bool_f"] = false;
+    ini["bar"]["bool_t"] = true;
+
+    rune::write_ini(ini, "usage.ini");
   }
 
-  file["bar"]["ghi"] = "lorem";
-  ASSERT_EQ(2, file.size());
-  ASSERT_TRUE(file.contains("foo"));
-  ASSERT_TRUE(file.contains("bar"));
+  {
+    const auto ini = rune::read_ini("usage.ini").value();
 
-  file.erase("foo");
-  ASSERT_EQ(1, file.size());
-  ASSERT_FALSE(file.contains("foo"));
-  ASSERT_TRUE(file.contains("bar"));
+    ASSERT_TRUE(ini.at("foo").at("positive").is_int());
+    ASSERT_EQ(9'394, ini.at("foo").at("positive").as<int>());
+
+    ASSERT_TRUE(ini.at("foo").at("negative").is_int());
+    ASSERT_EQ(-834, ini.at("foo").at("negative").as<int>());
+
+    ASSERT_TRUE(ini.at("foo").at("unsigned").is_uint());
+    ASSERT_EQ(734u, ini.at("foo").at("unsigned").as<unsigned>());
+
+    ASSERT_TRUE(ini.at("foo").at("str").is_string());
+    ASSERT_EQ("foobar", ini.at("foo").at("str").as<std::string>());
+
+    ASSERT_TRUE(ini.at("bar").at("real_1").is_float());
+    ASSERT_EQ(1.0f, ini.at("bar").at("real_1").as<float>());
+
+    ASSERT_TRUE(ini.at("bar").at("real_2").is_float());
+    ASSERT_EQ(87.3f, ini.at("bar").at("real_2").as<float>());
+
+    ASSERT_TRUE(ini.at("bar").at("real_3").is_float());
+    ASSERT_EQ(0.0f, ini.at("bar").at("real_3").as<float>());
+
+    ASSERT_TRUE(ini.at("bar").at("bool_f").is_bool());
+    ASSERT_EQ(false, ini.at("bar").at("bool_f").as<bool>());
+
+    ASSERT_TRUE(ini.at("bar").at("bool_t").is_bool());
+    ASSERT_EQ(true, ini.at("bar").at("bool_t").as<bool>());
+  }
 }
