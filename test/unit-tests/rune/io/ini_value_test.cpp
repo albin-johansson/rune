ac6bb3816078ce63ab3289a477f453dfa59e35ca
Albin Johansson <albinoscar99@hotmail.com>
2021-08-11 22:47:09 +0200

    Overhaul basic_ini_value class

--- test/unit-tests/rune/io/ini_value_test.cpp
+++ test/unit-tests/rune/io/ini_value_test.cpp
@@ -2,67 +2,62 @@
 
 #include <gtest/gtest.h>
 
+#include <iostream>  // cout
+
 TEST(IniValue, Defaults)
 {
   const rune::ini_value value;
   ASSERT_TRUE(value.is_string());
 }
 
+TEST(IniValue, DirectInitialization)
+{
+  const rune::ini_value str{"foo"};
+  ASSERT_TRUE(str.is_string());
+  ASSERT_EQ("foo", str);
+}
+
 TEST(IniValue, StringValue)
 {
   const rune::ini_value value = "foo";
   ASSERT_TRUE(value.is_string());
-
-  std::string str;
-  ASSERT_NO_THROW(value.get_to(str));
-  ASSERT_EQ("foo", str);
+  ASSERT_EQ("foo", value);
 }
 
 TEST(IniValue, SignedNegativeIntegerValue)
 {
   const rune::ini_value value = -123;
   ASSERT_TRUE(value.is_int());
-
-  int i{};
-  ASSERT_NO_THROW(value.get_to(i));
-  ASSERT_EQ(-123, i);
+  ASSERT_FALSE(value.is_uint());
+  ASSERT_EQ(-123, value);
 }
 
 TEST(IniValue, SignedPositiveIntegerValue)
 {
   const rune::ini_value value = 123'456;
   ASSERT_TRUE(value.is_int());
-
-  int i{};
-  ASSERT_NO_THROW(value.get_to(i));
-  ASSERT_EQ(123'456, i);
+  ASSERT_FALSE(value.is_uint());
+  ASSERT_EQ(123'456, value);
 }
 
 TEST(IniValue, UnsignedIntegerValue)
 {
-  const rune::ini_value value = 1'500'000u;
+  const rune::ini_value value = 1'563'329u;
   ASSERT_TRUE(value.is_uint());
-
-  rune::uint u{};
-  ASSERT_NO_THROW(value.get_to(u));
-  ASSERT_EQ(1'500'000u, u);
+  ASSERT_FALSE(value.is_int());
+  ASSERT_EQ(1'563'329u, value);
 }
 
 TEST(IniValue, BooleanValue)
 {
-  const rune::ini_value trueValue = true;
-  const rune::ini_value falseValue = false;
+  const rune::ini_value a{true};
+  const rune::ini_value b{false};
 
-  ASSERT_TRUE(trueValue.is_bool());
-  ASSERT_TRUE(falseValue.is_bool());
+  ASSERT_TRUE(a.is_bool());
+  ASSERT_TRUE(b.is_bool());
 
-  bool t{};
-  bool f{};
-  ASSERT_NO_THROW(trueValue.get_to(t));
-  ASSERT_NO_THROW(falseValue.get_to(f));
-
-  ASSERT_TRUE(t);
-  ASSERT_FALSE(f);
+  ASSERT_EQ(true, a);
+  ASSERT_EQ(false, b);
 }
 
 TEST(IniValue, GetToBool)
@@ -199,46 +194,9 @@ TEST(IniValue, GetToUnsignedIntStrongType)
   ASSERT_EQ(strong_uint{8'234}, u);
 }
 
-TEST(IniValue, GetToFloatStrongType)
+TEST(IniValue, TryGet)
 {
-  using strong_float = nenya::strong_type<float, struct strong_float_tag>;
-
-  const rune::ini_value value = 93.5;
-
-  strong_float f;
-  value.get_to(f);
-
-  ASSERT_EQ(strong_float{93.5f}, f);
-}
-
-TEST(IniValue, GetToBoolStrongType)
-{
-  using strong_bool = nenya::strong_type<bool, struct strong_bool_tag>;
-
-  const rune::ini_value value = true;
-
-  strong_bool b;
-  value.get_to(b);
-
-  ASSERT_EQ(strong_bool{true}, b);
-}
-
-TEST(IniValue, GetToStringStrongType)
-{
-  using strong_string = nenya::strong_type<std::string, struct strong_string_tag>;
-
-  const rune::ini_value value = "foobar";
-
-  strong_string s;
-  value.get_to(s);
-
-  ASSERT_EQ(strong_string{"foobar"}, s);
-}
-
-TEST(IniValue, TryGetString)
-{
-  rune::ini_value value = 123;
-  ASSERT_FALSE(value.try_get_string());
+  rune::ini_value value;
 
   value = "foo";
   ASSERT_TRUE(value.try_get_string());
@@ -247,50 +205,94 @@ TEST(IniValue, TryGetString)
   ASSERT_FALSE(value.try_get_float());
   ASSERT_FALSE(value.try_get_bool());
 
-  ASSERT_EQ("foo", *value.try_get_string());
-}
-
-TEST(IniValue, TryGetInt)
-{
-  rune::ini_value value = true;
-  ASSERT_FALSE(value.try_get_int());
-
   value = 123;
+  ASSERT_FALSE(value.try_get_string());
   ASSERT_TRUE(value.try_get_int());
   ASSERT_FALSE(value.try_get_uint());
   ASSERT_FALSE(value.try_get_float());
-  ASSERT_FALSE(value.try_get_string());
   ASSERT_FALSE(value.try_get_bool());
 
-  ASSERT_EQ(123, *value.try_get_int());
-}
-
-TEST(IniValue, TryGetUint)
-{
-  rune::ini_value value = "abc";
-  ASSERT_FALSE(value.try_get_uint());
-
-  value = 1'337u;
-  ASSERT_TRUE(value.try_get_uint());
+  value = 42u;
+  ASSERT_FALSE(value.try_get_string());
   ASSERT_FALSE(value.try_get_int());
+  ASSERT_TRUE(value.try_get_uint());
   ASSERT_FALSE(value.try_get_float());
-  ASSERT_FALSE(value.try_get_string());
   ASSERT_FALSE(value.try_get_bool());
-
-  ASSERT_EQ(1'337u, *value.try_get_uint());
-}
-
-TEST(IniValue, TryGetFloat)
-{
-  rune::ini_value value = 42;
-  ASSERT_FALSE(value.try_get_float());
 
   value = 12.3;
-  ASSERT_TRUE(value.try_get_float());
-  ASSERT_FALSE(value.try_get_uint());
-  ASSERT_FALSE(value.try_get_int());
   ASSERT_FALSE(value.try_get_string());
+  ASSERT_FALSE(value.try_get_int());
+  ASSERT_FALSE(value.try_get_uint());
+  ASSERT_TRUE(value.try_get_float());
   ASSERT_FALSE(value.try_get_bool());
 
-  ASSERT_EQ(12.3, *value.try_get_float());
+  value = true;
+  ASSERT_FALSE(value.try_get_string());
+  ASSERT_FALSE(value.try_get_int());
+  ASSERT_FALSE(value.try_get_uint());
+  ASSERT_FALSE(value.try_get_float());
+  ASSERT_TRUE(value.try_get_bool());
+}
+
+TEST(IniValue, TypeIndicators)
+{
+  rune::ini_value value = "foo";
+  ASSERT_TRUE(value.is_string());
+  ASSERT_FALSE(value.is_int());
+  ASSERT_FALSE(value.is_uint());
+  ASSERT_FALSE(value.is_float());
+  ASSERT_FALSE(value.is_bool());
+
+  value = 27;
+  ASSERT_FALSE(value.is_string());
+  ASSERT_TRUE(value.is_int());
+  ASSERT_FALSE(value.is_uint());
+  ASSERT_FALSE(value.is_float());
+  ASSERT_FALSE(value.is_bool());
+
+  value = 452u;
+  ASSERT_FALSE(value.is_string());
+  ASSERT_FALSE(value.is_int());
+  ASSERT_TRUE(value.is_uint());
+  ASSERT_FALSE(value.is_float());
+  ASSERT_FALSE(value.is_bool());
+
+  value = 542.8f;
+  ASSERT_FALSE(value.is_string());
+  ASSERT_FALSE(value.is_int());
+  ASSERT_FALSE(value.is_uint());
+  ASSERT_TRUE(value.is_float());
+  ASSERT_FALSE(value.is_bool());
+
+  value = false;
+  ASSERT_FALSE(value.is_string());
+  ASSERT_FALSE(value.is_int());
+  ASSERT_FALSE(value.is_uint());
+  ASSERT_FALSE(value.is_float());
+  ASSERT_TRUE(value.is_bool());
+}
+
+TEST(IniValue, Comparison)
+{
+  ASSERT_EQ(rune::ini_value{}, rune::ini_value{});
+
+  ASSERT_EQ(rune::ini_value{42}, rune::ini_value{42});
+  ASSERT_NE(rune::ini_value{42}, rune::ini_value{42u});
+  ASSERT_NE(rune::ini_value{42}, rune::ini_value{123});
+
+  ASSERT_NE(rune::ini_value{123u}, rune::ini_value{"foo"});
+  ASSERT_EQ(rune::ini_value{12.3}, rune::ini_value{12.3});
+
+  ASSERT_EQ(rune::ini_value{true}, rune::ini_value{true});
+  ASSERT_EQ(rune::ini_value{false}, rune::ini_value{false});
+  ASSERT_NE(rune::ini_value{true}, rune::ini_value{false});
+}
+
+TEST(IniValue, StreamOperator)
+{
+  std::cout << "rune::ini_value{42}: " << rune::ini_value{42} << '\n';
+  std::cout << "rune::ini_value{123u}: " << rune::ini_value{123u} << '\n';
+  std::cout << "rune::ini_value{27.5}: " << rune::ini_value{27.5} << '\n';
+  std::cout << "rune::ini_value{\"foo\"}: " << rune::ini_value{"foo"} << '\n';
+  std::cout << "rune::ini_value{true}: " << rune::ini_value{true} << '\n';
 }
