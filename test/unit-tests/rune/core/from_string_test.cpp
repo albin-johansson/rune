c9b277d81cd2653fbccc8c86f028f4af7ac8d26b
Albin Johansson <albinoscar99@hotmail.com>
2021-08-18 19:57:19 +0200

    Improve from_string function

---
+++ test/unit-tests/rune/core/from_string_test.cpp
@@ -0,0 +1,23 @@
+#include "core/from_string.hpp"
+
+#include <gtest/gtest.h>
+
+#include "aliases/integers.hpp"
+
+TEST(FromString, Integer)
+{
+  ASSERT_FALSE(rune::from_string<int>("foo"));
+
+  ASSERT_EQ(42, rune::from_string<int>("42"));
+  ASSERT_EQ(-123, rune::from_string<int>("-123"));
+  ASSERT_EQ(234, rune::from_string<rune::uint8>("234"));
+}
+
+TEST(FromString, Float)
+{
+  ASSERT_FALSE(rune::from_string<float>("bar"));
+
+  ASSERT_EQ(12.3, rune::from_string<double>("12.3"));
+  ASSERT_EQ(932.0f, rune::from_string<float>("932"));
+  ASSERT_EQ(-83.4f, rune::from_string<float>("-83.4"));
+}
