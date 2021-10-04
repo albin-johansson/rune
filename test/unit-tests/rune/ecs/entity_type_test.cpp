3c0bb4affa787010c5851c7560c00c28c4aa6d9d
Albin Johansson <albinoscar99@hotmail.com>
2021-10-04 22:24:05 +0200

    Remove ECS folder

--- test/unit-tests/rune/ecs/entity_type_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "ecs/entity_type.hpp"
-
-#include <gtest/gtest.h>
-
-#include <concepts>  // same_as
-
-using foo = rune::entity_type<struct foo_tag>;
-
-static_assert(std::same_as<entt::entity, foo::value_type>);
